836f76f51a12bd53a62e7d1a298e311d194b2b4e
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-02-14 17:39:32 +0530

    Add files via upload

---
+++ ModDivmod.py
@@ -0,0 +1,12 @@
+"""
+Title     : Mod Divmod
+Subdomain : Math
+Domain    : Python
+"""
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+d = divmod(a, b)
+print(d[0])
+print(d[1])
+print(d)
